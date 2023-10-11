aa17a6bea54e9cb863f009246d574de57649ff0d
Mykaela Wallen <mykaelawallen@gmail.com>
2023-10-11 09:14:02 -0500

    Initial Commit

---
+++ classmateApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  classmateApp
+//
+//  Created by MYKAELA WALLEN on 10/11/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
